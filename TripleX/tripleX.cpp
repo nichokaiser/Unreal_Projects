5360021009b89d22ed017e36a87e713ba70757da
Nicholas Kaiser <NicholasKaiser@nicholass-air.attlocal.net>
2021-01-08 19:58:56 -0800

    finished tripleX game

--- TripleX/tripleX.cpp
+++ TripleX/tripleX.cpp
@@ -1,19 +1,67 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-int main() {
-    cout << "You are a secret agent breaking into a secure server room...\n";
-    cout << "You need to enter the correct codes to continue...\n";
+void PrintIntroduction(int CurrentLevel);
+bool PlayGame(int CurrentLevel);
 
-    int code_numb_1 = 4;
-    int code_numb_2 = 3;
-    int code_numb_3 = 2;
-    int code_numb_sum = code_numb_1 + code_numb_2 + code_numb_3;
-    int code_numb_prod = code_numb_1 * code_numb_2 * code_numb_3;
+int main(void) {
+    srand(time(NULL));
 
-    cout << code_numb_sum << endl;
-    cout << code_numb_prod << endl;
+    int LevelDifficulty = 1;
+    const int MaxLevels = 3;
+
+    while (LevelDifficulty <= MaxLevels) {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        cin.clear();
+        cin.ignore();
+
+        if (bLevelComplete) {
+            LevelDifficulty++;
+        }   
+    }
+
+    cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
 
     return 0;
+}
+
+void PrintIntroduction(int CurrentLevel) {
+    cout << "\nYou are a secret agent breaking into a level " << CurrentLevel << " secure server room...\n";
+    cout << "You need to enter the correct codes to continue...\n\n";
+}
+
+bool PlayGame(int CurrentLevel) {
+    bool bPlayerWon = false;
+
+    PrintIntroduction(CurrentLevel);
+
+    const int CodeNumber1 = (rand() % CurrentLevel) + CurrentLevel;
+    const int CodeNumber2 = (rand() % CurrentLevel) + CurrentLevel;
+    const int CodeNumber3 = (rand() % CurrentLevel) + CurrentLevel;
+    const int CodeNumberSum = CodeNumber1 + CodeNumber2 + CodeNumber3;
+    const int CodeNumberProduct = CodeNumber1 * CodeNumber2 * CodeNumber3;
+
+    cout << "+ There are 3 numbers in the code\n";
+    cout << "+ The numbers add up to: " << CodeNumberSum << endl;
+    cout << "+ The numbers multiply to give: " << CodeNumberProduct << "\n" << endl;
+
+    int PlayerGuess1, PlayerGuess2, PlayerGuess3;
+
+    cin >> PlayerGuess1 >> PlayerGuess2 >> PlayerGuess3;
+
+    int GuessSum = PlayerGuess1 + PlayerGuess2 + PlayerGuess3;
+    int GuessProduct = PlayerGuess1 * PlayerGuess2 * PlayerGuess3;
+
+    if (GuessSum == CodeNumberSum && GuessProduct == CodeNumberProduct) {
+        cout << "\n*** Well done agent! You have extracted a file! Keep going! ***\n";
+        bPlayerWon = true;
+    }
+    else {
+        cout << "\n*** You entered the wrong code! Careful agent! Try again! ***\n";
+        bPlayerWon = false;
+    }
+
+    return bPlayerWon;
 }
